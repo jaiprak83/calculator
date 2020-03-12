5493c620d84cb1d5c9c94ab4f404868270d2d08a
jaiprak83 <62099149+jaiprak83@users.noreply.github.com>
2020-03-12 22:08:01 +1100

    Fixed by the instructions

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -12,7 +12,7 @@ exports.calculate = function(req, res) {
 
    // Missed the change on this line first time.
   var operations = {
-    'add':      function(a,b) { return a + b },
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
